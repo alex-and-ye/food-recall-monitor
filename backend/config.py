1792cfa70bf895a7f57d95ff598178dc58d083f0
im-varun <vrun.mulchandani@gmail.com>
2026-06-07 20:25:18 -0500

    Add db and services dependencies

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,19 @@
-def get_db():
-    db_instance = "Dummy database connection"
-    try:
-        yield db_instance
-    finally:
-        pass
+import os
+
+from fastapi import Depends
+
+from db.interface import FoodRecallAlertsDBInterface
+from db.chroma_client import FoodRecallAlertsChromaClient
+from services.alerts_service import AlertsService
+from services.pipeline_service import PipelineService
+
+_db_client = FoodRecallAlertsChromaClient(os.path.join(os.getcwd(), "data", "chroma_db"))
+
+def get_db() -> FoodRecallAlertsDBInterface:
+    return _db_client
+
+def get_alerts_service(db: FoodRecallAlertsDBInterface = Depends(get_db)) -> AlertsService:
+    return AlertsService(db)
+
+def get_pipeline_service(db: FoodRecallAlertsDBInterface = Depends(get_db)) -> PipelineService:
+    return PipelineService(db)
