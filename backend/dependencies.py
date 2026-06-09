7e719500ad63c4b00e354d8c661215175143dfa8
im-varun <vrun.mulchandani@gmail.com>
2026-06-09 13:39:22 -0500

    Change db client variable name

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -7,10 +7,10 @@ from db.chroma_client import FoodRecallAlertsChromaClient
 from services.alerts import AlertsService
 from services.pipeline import PipelineService
 
-_db_client: FoodRecallAlertsDBInterface = FoodRecallAlertsChromaClient(os.path.join(os.getcwd(), "data", "chroma_db"))
+_chroma_client: FoodRecallAlertsDBInterface = FoodRecallAlertsChromaClient(os.path.join(os.getcwd(), "data", "chroma_db"))
 
 def get_db() -> FoodRecallAlertsDBInterface:
-    return _db_client
+    return _chroma_client
 
 def get_alerts_service(db: FoodRecallAlertsDBInterface = Depends(get_db)) -> AlertsService:
     return AlertsService(db)
